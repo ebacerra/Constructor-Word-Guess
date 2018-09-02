07db7092ea6e8024b6b60e4df5e8eca7e53bfc12
Ellen <ellen.bacerra@gmail.com>
2018-09-02 16:34:40 -0500

    update

---
+++ Word.js
@@ -0,0 +1,15 @@
+var letter = require("./Letter.js");// depends on the Letter Constructor
+console.log(letter);
+
+
+function Word(wrd) {
+    // made another constructor here with listofWords and an empty array.
+    this.listOfWords = wrd;
+    this.arrayList = [];
+    this.lettersOfobject = function () {
+
+    }
+
+
+
+};
